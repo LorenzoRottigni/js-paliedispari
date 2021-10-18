8a1f79ad9f1d6218c7860e082f5a4d44ca7218ab
LorenzoRottigni <lorenzo@rottigni.net>
2021-10-18 17:16:05 +0200

    Interfaccia completa, codice commentato

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const inputWord = prompt('insert a word: ');
+//get button
+const button = document.querySelector('button');
+//get text field
+const textField = document.querySelector('input');
+//get output h1
+const outputH1 = document.querySelector('h1');
 
 function isPalindrome(word){
+    //new array with reverse indexes
     let orderedArray = "";
+    //add chars to orderedArray from the last char of the given word
     for (let i = word.length; i > 0; i--) {
         orderedArray = orderedArray + word[i - 1];
     }
+    //if old word and new word are equals
     if(orderedArray === word){
         return true;
     }else{
@@ -12,8 +20,19 @@ function isPalindrome(word){
     }
 }
 
-if(isPalindrome(inputWord)){
-    document.write(`${inputWord}  is a palindrome`);
-}else{
-    document.write(`${inputWord}  is not palindrome`);
-}
+button.addEventListener("click", function(){
+    //get inserted word
+    const textFieldValue = textField.value;
+    //error handler
+    if(textFieldValue === ""){
+        outputH1.textContent = 'Insert a valid word'
+    }else{//if no errors
+        if(isPalindrome(textFieldValue)){//if the word is a palindrome
+            outputH1.innerHTML = `<span class="text-secondary fw-5">${textFieldValue}</span>  is a palindrome`;
+        }else{//if the word is not a palindrome
+            outputH1.innerHTML = `<span class="text-secondary fw-5">${textFieldValue}</span>  is not a palindrome`;
+        }
+    }
+    //reset after click
+    textField.value = null;
+});
